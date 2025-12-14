e48783e4ba3c2b53a7e4cf2cc80be77f519887e6
srashtiparmar1234gmailcom <srashtiparmar1234@gmail.com>
2025-12-14 08:57:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -129,7 +129,10 @@ app.get("/"),(req,res) => {
   res.send("Skedbord Play Park Backend is Running");
     });
 // ===== START SERVER =====
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log('Server running on port ',PORT);
 });
+
 
